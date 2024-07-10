cae581fb0c9bc9b029fb2c649e3868da3fc4dc56
Vinay Chawla <vinay.chawla11@gmail.com>
2024-07-10 16:12:12 +0530

    print

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     requests = server_socket.accept() # wait for client
-    print(requests)
+    print(requests[0])
 
 if __name__ == "__main__":
     main()
