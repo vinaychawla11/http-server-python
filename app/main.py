623fdb4b33b8f7231eb63be9d891c0de92944fe5
Vinay Chawla <vinay.chawla11@gmail.com>
2024-07-10 17:17:36 +0530

    Threading

--- app/main.py
+++ app/main.py
@@ -5,27 +5,37 @@ import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connect, address = server_socket.accept() # wait for client
+    while True:
+        connect, address = server_socket.accept() # wait for client
+        client_thread = threading.Thread(target=handle_client, args=(connect,))
+        client_thread.start()
+
+def handle_client(connect):
     
-    data = connect.recv(1024).decode().split("\r\n")
-    method, path,_  = data[0].split()
-    if method == "GET":
-        if path == "/":
-            connect.sendall(response)
-        elif path.startswith("/echo/"):
-            pathArr = path.split("/")[-1]
-            response2 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ str(len(pathArr)) + "\r\n\r\n" + pathArr + "\r\n"
-            connect.sendall(response2.encode())
-        elif path == "/user-agent":
-            userAgent = data[2].split(":")[1].strip()
-            response2 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ str(len(userAgent)) + "\r\n\r\n" + userAgent + "\r\n"
-            connect.sendall(response2.encode())
+    try:
+        # Uncomment this to pass the first stage
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+        connect, address = server_socket.accept() # wait for client
+        data = connect.recv(1024).decode().split("\r\n")
+        method, path,_  = data[0].split()
+        if method == "GET":
+            if path == "/":
+                connect.sendall(response)
+            elif path.startswith("/echo/"):
+                pathArr = path.split("/")[-1]
+                response2 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ str(len(pathArr)) + "\r\n\r\n" + pathArr + "\r\n"
+                connect.sendall(response2.encode())
+            elif path == "/user-agent":
+                userAgent = data[2].split(":")[1].strip()
+                response2 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ str(len(userAgent)) + "\r\n\r\n" + userAgent + "\r\n"
+                connect.sendall(response2.encode())
         else:
             connect.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        connect.close()
 
 if __name__ == "__main__":
     main()
