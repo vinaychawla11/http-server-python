74a4606fc0d67c208d090db992925f52731e9862
Vinay Chawla <vinay.chawla11@gmail.com>
2024-07-10 19:15:38 +0530

    revert

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ def handle_client(connect):
             elif path.startswith("/echo/"):
                 pathArr = path.split("/")[-1]
                 if "gzip" in encoded:
-                    send_response(connect, "200 OK","gzip", "text/plain" , binascii.hexlify(str(gzip.compress(pathArr.encode()))).encode())
+                    send_response(connect, "200 OK","gzip", "text/plain" , str(gzip.compress(pathArr.encode())))
                 else:
                     send_response(connect, "200 OK",encoding, "text/plain" , pathArr)
             elif path == "/user-agent":
